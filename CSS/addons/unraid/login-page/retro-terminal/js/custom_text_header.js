ab3e358bda796b5c56e48e84fee8f62020521a92
TenAceousSpade <66499590+TenAceousSpade@users.noreply.github.com>
2021-11-22 06:33:22 -0500

    Update custom_text_header.js

--- CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ CSS/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,10 +6,11 @@
 // You must escape any backward slashes. "\\"
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>   _____      __  __    _________     
+<div class="custom-text-header"> <pre>   
+   _____      __  __    _________     
   / ___/___  / /_/ /_  / ____/ (_)  __
   \__ \/ _ \/ __/ __ \/ /_  / / / |/_/
- ___/ /  __/ /_/ / / / __/ / / />  <  ,&lt;
+ ___/ /  __/ /_/ / / / __/ / / />  <,&lt;
 /____/\___/\__/_/ /_/_/   /_/_/_/|_|  
                                       </pre> </div>
 `;
